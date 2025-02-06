1d6a870e41fc0b5885ff6cf07540f70cd638d1e2
arushig2 <arushig223@gmail.com>
2025-02-07 00:08:52 +0530

    [Arushi Gupta] Add: Assignment-17 this, static and is operator used

---
+++ BankAccountSystem.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+
+//class BankAccount
+//{
+//    public static string bankName = "Bank Of India";
+//    public static int totalBankAccounts = 0;
+//    public string AccountHolderName;
+//    public readonly int AccountNumber;
+
+//    public BankAccount(string AccountHolderName, int AccountNumber)
+//    {
+//        this.AccountHolderName = AccountHolderName;
+//        this.AccountNumber = AccountNumber;
+//        totalBankAccounts++;
+//    }
+
+//    public static void GetTotalAccount()
+//    {
+//        Console.WriteLine($"Total Accounts in bank are: {totalBankAccounts}");
+//    }
+
+//    public void Display()
+//    {
+//        Console.WriteLine($"Bank Name: {bankName}");
+//        Console.WriteLine($"Account Holder name: {AccountHolderName}");
+//        Console.WriteLine($"Bank Account Number: {AccountNumber}");
+//    }    
+    
+//}
+
+//public class Temp
+//{
+//    public Temp() { }
+//}
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        BankAccount account1 = new BankAccount("Ravi", 453);
+//        account1.Display();
+//        // account1.AccountNumber = 543; //error as read only variable
+
+//        BankAccount account2 = new BankAccount("Sunita", 568);
+//        account2.Display();
+
+//        BankAccount.GetTotalAccount();
+
+//        Temp temp = new Temp();
+
+//        if (temp is BankAccount)
+//        {
+//            Console.WriteLine("An instance of BankAccount");
+//        } else
+//        {
+//            Console.WriteLine("Not an instance of BankAccount");
+//        }
+//    }
+//}
+
---
+++ LibraryManagement.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+
+//public class Book
+//{
+//    public static string libraryName = "Books Cafe";
+//    public string title;
+//    public string author;
+//    public readonly long isbn;
+
+//    public Book()
+//	{
+//	}
+
+//    public Book(string title, string author, long isbn)
+//    {
+//        this.title = title;
+//        this.author = author;
+//        this.isbn = isbn;
+//    }
+
+//    public static void DisplayLibraryName()
+//    {
+//        Console.WriteLine("Library Name: ");
+//    }
+
+//    public void Display()
+//    {
+//        Console.WriteLine($"Library Name: {libraryName}");
+//        Console.WriteLine($"Book: {title}");
+//        Console.WriteLine($"Author: {author}");
+//        Console.WriteLine($"ISBN: {isbn}");
+//    }
+//}
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//       Book book1 = new Book("Atomic Habits", "James Clear", 12345678);
+//       book1.Display();
+
+//       //book1.isbn = 23456789; //error
+
+//       Book book2 = new Book("Cant Hurt", "David Goggins", 34234453);
+//       book2.Display();
+//    }
+//}
+
+

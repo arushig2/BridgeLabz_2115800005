6cc90d8db8722cc3e95781c04eff16a7440f2915
arushig2 <arushig223@gmail.com>
2025-03-02 22:27:42 +0530

    [Arushi Gupta] Add: Employee management project add Nlog

---
+++ EmployeeManagementProj/CommonLayer/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommonLayer
+{
+    public class Employee
+    {
+        [Key]  
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
